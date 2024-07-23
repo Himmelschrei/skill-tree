1cae2246e1483f2f84e254d076d3be566b54ccfe
Carlos Pineda <himmelschrei@gmail.com>
2024-07-23 18:26:14 +0200

    Keep organizing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [NavbarComponent],
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'skill-tree';
--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'help-component',
+  standalone: true,
   templateUrl: './help.component.html',
   styleUrls: ['./help.component.css'],
 })
