1be7799c6287c921314dca99a4d23b20331f202a
Carlos Pineda <himmelschrei@gmail.com>
2024-07-31 18:34:44 +0200

    Add relationship lines

--- src/app/stdiagram/stdiagram.component.ts
+++ src/app/stdiagram/stdiagram.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import skills from '../../assets/data/skills.json';
+import { Relationship } from '../relationship';
 import { Skill } from '../skill';
 
 @Component({
@@ -9,12 +10,23 @@ import { Skill } from '../skill';
   standalone: true,
   imports: [MatCardModule, CommonModule],
   templateUrl: './stdiagram.component.html',
-  styleUrl: './stdiagram.component.css',
+  styleUrls: ['./stdiagram.component.css'],
 })
 export class StDiagramComponent {
   public skills: Skill[];
+  public relationships: Relationship[];
 
   constructor() {
     this.skills = skills.skillData as Skill[];
+    this.relationships = skills.relationships as Relationship[];
+  }
+
+  public GetSkill(id: number): Skill | null {
+    return this.skills.find((sk) => sk.id === id) || null;
+  }
+
+  public getSkillPosition(id: number) {
+    const skill = this.GetSkill(id);
+    return skill ? { x: skill.xpos, y: skill.ypos } : { x: 0, y: 0 };
   }
 }
